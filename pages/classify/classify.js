69faeedaf7b4e593badda1c5fe060fc9d24f72cf
Emmmchanchanman <31648615+Emmmchanchanman@users.noreply.github.com>
2018-06-24 03:11:04 +0800

    Create classify.js

---
+++ pages/classify/classify.js
@@ -0,0 +1,81 @@
+// pages/classify/classify.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var _This = this;
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5b28be4b8d769778f81a0aa2/snail/classify',
+      success: function (res) {
+        var cData = res.data.cData;
+        console.log(cData);
+        _This.setData({
+          classifyData: cData
+        })
+      }
+    })
+  },
+  onClassifyTap:function(ev){
+    var bId = ev.currentTarget.dataset.bookid;
+    wx.navigateTo({
+      url: 'classify-books/classify-books?id='+bId,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
