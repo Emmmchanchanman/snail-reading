19004584b1c3f45ecd5c207e90f84f7ca276d614
Emmmchanchanman <31648615+Emmmchanchanman@users.noreply.github.com>
2018-06-24 03:08:56 +0800

    Create read-detail.js

---
+++ pages/read/read-detail/read-detail.js
@@ -0,0 +1,71 @@
+// pages/read/read-detail/read-detail.js
+var artD = require("../../../data/listData.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // console.log(options);
+    var dId = options.id;
+    this.setData({
+      detailData:artD[dId]
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
